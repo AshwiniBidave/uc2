1ea12eeb4336c049d739d2509eb376427fe03fa9
AshwiniBidave <ashwinibidave1993@gmail.com>
2022-07-26 15:28:04 +0530

    upload file uc1

---
+++ EmployeePOrA/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace EmployeePOrA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int empcheck;
+            int Emp_Rate_per_hour = 20;
+            int empHrs = 0;
+            int empWage = 0;
+
+            int Is_Full_Time = 1;
+            Random random = new Random();
+            empcheck = random.Next(0, 2);
+
+            if (empcheck == Is_Full_Time)
+            {
+                empHrs = 8;
+                Console.WriteLine("Employyee is present");
+            }
+            else
+            {
+                  
+                Console.WriteLine("Employee is Absent");
+
+            }
+            empWage = empHrs * Emp_Rate_per_hour;
+            Console.WriteLine("empwage is :" + empWage);
+
+        }
+    }
+}
